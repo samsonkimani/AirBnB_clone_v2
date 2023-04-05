d200d35b6e108e7c253b266923382b3f1941e8a1
samsonkimani <samsonkimani43@gmail.com>
2023-04-05 20:35:06 +0300

    amending the sed command options

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -26,7 +26,7 @@ sudo ln -sf /data/web_static/releases/test /data/web_static/current
 sudo chown -hR ubuntu:ubuntu /data/
 
 # Update Nginx configuration to serve content from /data/web_static/current
-sudo sed -i '/index.html index.htm index.nginx-debian.html;/a \\n\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-available/default
+sudo sed -i '38i\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-available/default
 
 
 # Restart Nginx

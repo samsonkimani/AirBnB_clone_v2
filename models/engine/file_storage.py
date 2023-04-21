3d2c6360462a9f29dfd0254951500b51d57be970
samsonkimani <samsonkimani43@gmail.com>
2023-04-21 16:15:15 +0300

    ammending engine files to enable session reload

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -63,3 +63,7 @@ class FileStorage:
             key = obj.__class__.__name__+'.'+obj.id
             if key in self.__objects:
                 del self.__objects[key]
+
+    def close(self):
+        """ a method close"""
+        Filestorage.reload()
